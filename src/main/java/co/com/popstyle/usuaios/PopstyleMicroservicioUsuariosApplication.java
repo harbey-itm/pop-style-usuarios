14fcfd047362c0875c9700b1697cfde54e618d41
Harbey Brieceno <harbeybriceno329728@correo.itm.edu.co>
2023-05-09 16:49:41 -0500

    Inicio Microservicio Usuarios

---
+++ src/main/java/co/com/popstyle/usuaios/PopstyleMicroservicioUsuariosApplication.java
@@ -0,0 +1,13 @@
+package co.com.popstyle.usuaios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopstyleMicroservicioUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopstyleMicroservicioUsuariosApplication.class, args);
+	}
+
+}
